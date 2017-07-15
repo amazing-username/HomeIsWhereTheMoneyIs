15070abfd0827adfd07db8942533549a7daaa5e5
amazing-username <kdeng02@aurora.edu>
2017-07-15 11:51:31 -0500

    Database backup

--- maxipad.sql
+++ maxipad.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.16  Distrib 10.1.23-MariaDB, for Linux (x86_64)
+-- MySQL dump 10.16  Distrib 10.1.25-MariaDB, for Linux (x86_64)
 --
 -- Host: localhost    Database: maxipad
 -- ------------------------------------------------------
--- Server version	10.1.23-MariaDB
+-- Server version	10.1.25-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -34,7 +34,7 @@ CREATE TABLE `bills` (
 
 LOCK TABLES `bills` WRITE;
 /*!40000 ALTER TABLE `bills` DISABLE KEYS */;
-INSERT INTO `bills` VALUES ('Fish','juice'),('Buthole','juice'),('car','juice'),('Beef','weezybaby'),('Ink','weezybaby'),('Pepsi','weezybaby');
+INSERT INTO `bills` VALUES ('Meanhead','polohatwearin'),('Weights','polohatwearin'),('internet','polohatwearin'),('water','polohatwearin');
 /*!40000 ALTER TABLE `bills` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -107,7 +107,7 @@ CREATE TABLE `responsible` (
 
 LOCK TABLES `responsible` WRITE;
 /*!40000 ALTER TABLE `responsible` DISABLE KEYS */;
-INSERT INTO `responsible` VALUES (NULL,NULL,'ice',NULL,NULL,NULL),('201705Fish','Fish','weezybaby','6','10','juice'),('201705Fish','Fish','weezybaby','6','10','juice'),('201705Fish','Fish','weezybaby','6','10','juice'),('201705Fish','Fish','weezybaby','6','10','juice'),('201705Fish','Fish','bbc_champ','6','10','juice'),('201705Pepsi','Pepsi','juice','16','20','weezybaby');
+INSERT INTO `responsible` VALUES ('201706Meanhead','Meanhead','TexasJiveTurkey','10','50','polohatwearin'),('201706Weights','Weights','jonmartinez2018','50','50','polohatwearin'),('201706internet','internet','polohatwearin','0','16.8','polohatwearin'),('201706Meanhead','Meanhead','TexasJiveTurkey','0','50','polohatwearin'),('201706internet','internet','TexasJiveTurkey','0','16.8','polohatwearin'),('201706internet','internet','jonmartinez2018','0','16.8','polohatwearin'),('201706internet','internet','kmcc','0','16.8','polohatwearin'),('201706internet','internet','brucebanner','0','16.8','polohatwearin'),('201703internet','internet','jonmartinez2018','11','12','polohatwearin'),('201703water','water','jonmartinez2018','0','17','polohatwearin');
 /*!40000 ALTER TABLE `responsible` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -134,7 +134,7 @@ CREATE TABLE `transactions` (
 
 LOCK TABLES `transactions` WRITE;
 /*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
-INSERT INTO `transactions` VALUES ('Fish','201705','30','201705Fish','juice','3'),('car','201705','400','201705car','juice','2'),('car','201705','400','201705car','juice','2'),('Beef','201705','30','201705Beef','weezybaby','5'),('Ink','201705','87','201705Ink','weezybaby','3'),('Pepsi','201705','100','201705Pepsi','weezybaby','5');
+INSERT INTO `transactions` VALUES ('Meanhead','201706','100','201706Meanhead','polohatwearin','2'),('Weights','201706','100','201706Weights','polohatwearin','2'),('internet','201706','84','201706internet','polohatwearin','5'),('internet','201703','12','201703internet','polohatwearin','1'),('water','201703','17','201703water','polohatwearin','1');
 /*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -160,7 +160,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('Jake','Johnson','juice','grass','Single'),('Dwayne','Carter','weezybaby','fireman','single'),('Bob','The Builder','cantfix','fatone','double'),('Jessica','Bangkok','bbc_champ','wife','double');
+INSERT INTO `users` VALUES ('Kun','Deng','polohatwearin','green','double'),('Austin','Tolbert','TexasJiveTurkey','Ninja007','double'),('Jon','Martinez','jonmartinez2018','Spartan3','single'),('Kendall','McCarter','kmcc','grass','single'),('Kevin','Crozier','brucebanner','hulk','single');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -173,4 +173,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-05-29 22:30:58
+-- Dump completed on 2017-07-15 11:51:12
